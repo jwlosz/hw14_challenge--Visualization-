6da524d2c1e1c5c02702d9d5afc59895a3f722f3
jwlosz <jwloszek97@gmail.com>
2024-07-31 20:15:09 -0400

    initial submission

--- Submission Code/static/js/app.js
+++ Submission Code/static/js/app.js
@@ -3,21 +3,23 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let info = metadata.filter(x => x.id == sample)[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
-  });
+    for (const [key, value] of Object.entries(info)) {
+      panel.append("h6").text(`${key}: ${value}`);
+    }
+    });
 }
 
 // function to build both charts
